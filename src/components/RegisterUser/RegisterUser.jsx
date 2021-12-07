f97271630e468d24addada17047a09d80fb48213
Roberto B. Costa <romeromatallica@gmail.com>
2021-12-06 21:19:00 -0300

    Criei as funções para guardar os valores dos inputs ao click do botão

--- src/components/RegisterUser/RegisterUser.jsx
+++ src/components/RegisterUser/RegisterUser.jsx
@@ -2,20 +2,49 @@ import { RegisterUserStyled } from "./RegisterUserStyled";
 import { InputStyled } from "../InputsStyled";
 import { ButtonStyled } from "../ButtonStyled";
 import { TitleStyled } from "../TitleStyled";
+import { useState } from "react";
 
 export const RegisterUser = () => {
+
+    const [values, setValues] = useState();
+
+    const handleChangeValues = (e) => {
+        setValues(prevValue=>({
+            ...prevValue,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log(values);
+    }
+
     return (
         <>
             <TitleStyled>Game Shop</TitleStyled>
-                <RegisterUserStyled id="RegisterUserForm">
-                    <label htmlFor="name">Nome: </label>
-                    <InputStyled type="text" name="name" id="name" />
-                    <label htmlFor="cost">Preço: </label>
-                    <InputStyled type="text" name="cost" id="cost" />
-                    <label htmlFor="amount">Quantidade: </label>
-                    <InputStyled type="text" name="amount" id="amount" />
-                    <ButtonStyled>Cadastrar</ButtonStyled>
-                </RegisterUserStyled>
+            <RegisterUserStyled id="RegisterUserForm">
+                <label htmlFor="name">Nome: </label>
+                <InputStyled 
+                type="text" 
+                name="name" 
+                id="name" 
+                onChange={ handleChangeValues } />
+
+                <label htmlFor="cost">Preço: </label>
+                <InputStyled type="text" 
+                name="cost" 
+                id="cost" 
+                onChange={ handleChangeValues } />
+
+                <label htmlFor="amount">Quantidade: </label>
+                <InputStyled type="text" 
+                name="amount" 
+                id="amount" 
+                onChange={ handleChangeValues } />
+
+                <ButtonStyled onClick={(e) => handleClick(e)}>Cadastrar</ButtonStyled>
+            </RegisterUserStyled>
         </>
     );
 };
